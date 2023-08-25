cb88c9c6d436692f5106b7ac4a73e55355ce9d60
esathaiah <esathaiah@US-5CD9288VDN>
2023-08-25 19:07:18 +0530

    Add project files.

---
+++ DAL/AppDBContext.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext() { }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        { 
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer((@"Server= US-5CD9288VDN;Database=MYDB;Integrated Security=True; trusted_connection=true; Encrypt=False;"));
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
